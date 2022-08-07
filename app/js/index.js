a48459850c5eb96148619644442bff0c85512042
Alison Caetano <78568924+AlizonCaetano@users.noreply.github.com>
2022-08-07 16:03:57 -0300

    Update index.js

--- app/js/index.js
+++ app/js/index.js
@@ -2,10 +2,10 @@ import { Router } from './router.js'
 
 const router = new Router()
 
-router.add('/', '../home.html')
-router.add('/universe', '../universe.html')
-router.add('/exploration', '../exploration.html')
-router.add('404', '../404.html')
+router.add('/', '../pages/home.html')
+router.add('/universe', '../pages/universe.html')
+router.add('/exploration', '../pages/exploration.html')
+router.add('404', '../pages/404.html')
 
 router.handle()
 
